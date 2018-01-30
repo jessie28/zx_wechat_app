37cb3cf6540c551d482ac2d6b3944cbcf817c367
zhangxin <xinzhang@www.mjitech.com>
2018-01-30 20:19:00 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+Page({
+    data: {
+        message:'hello world',
+        id:0,
+        condition:true,
+        flag:false,
+        zero : 0,
+        array:[
+            {
+                massage:'test'
+            },
+            {
+
+            }
+        ]
+    }
+})
